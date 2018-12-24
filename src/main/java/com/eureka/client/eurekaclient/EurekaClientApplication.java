76ce69c3a8fabb8a91855e3e57ba83abb2686a3b
SweetHomicide <mlgbcannon@sina.cn>
2018-12-24 11:09:06 +0800

    init commit

---
+++ src/main/java/com/eureka/client/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,26 @@
+package com.eureka.client.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home(){
+		return "hello world from port ";
+	}
+
+}
+
